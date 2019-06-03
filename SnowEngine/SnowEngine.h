16fdc7d5ee72df3b8891fd4c425f1b1a45b61083
Joe Milner-Moore <joemm@apple.com>
2019-06-03 17:18:13 +0100

    Initial Commit

---
+++ SnowEngine/SnowEngine.h
@@ -0,0 +1,19 @@
+//
+//  SnowEngine.h
+//  SnowEngine
+//
+//  Created by Joe Milner-Moore on 03/06/2019.
+//  Copyright © 2019 Snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SnowEngine.
+FOUNDATION_EXPORT double SnowEngineVersionNumber;
+
+//! Project version string for SnowEngine.
+FOUNDATION_EXPORT const unsigned char SnowEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SnowEngine/PublicHeader.h>
+
+
